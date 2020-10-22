ddf6dd351e5450bfb84f4e3f8b01c79124c3641a
Kamil Staniszewski <ender@Kamils-MacBook-Pro.local>
2020-10-22 18:14:57 +0200

    logs changes

--- apps/logs/models.py
+++ apps/logs/models.py
@@ -7,7 +7,7 @@ class WorkingChangeLogs(models.Model):
     workingtime = models.ForeignKey(WorkingTime, related_name='workinglogs', on_delete=models.CASCADE)
     time_start = models.CharField(max_length=50, default='')
     time_end = models.CharField(max_length=50, default='')
-    notes = models.CharField(max_length=100, null=False, blank=False)
+    notes = models.CharField(max_length=100, blank=False, default='')
     changed_by = models.CharField(max_length=60, default='')
     changed_at = models.CharField(max_length=50, default='')
 
